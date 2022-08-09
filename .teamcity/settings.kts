910c20f0b51dfa488c597b113789f2d7188d093e
Alistair.Young <alistair.young@xero.com>
2022-08-09 12:40:58 +1200

    added build feature

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.freeDiskSpace
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
@@ -56,6 +57,12 @@ object Anothertodolist_Build : BuildType({
             perCheckinTriggering = true
             groupCheckinsByCommitter = true
             enableQueueOptimization = false
+        }
+    }
+    features {
+        freeDiskSpace {
+            requiredSpace = "6gb"
+            failBuild = true
         }
     }
 })
