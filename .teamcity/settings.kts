3dd7551fe2fba618b83cf103ec9dd4185e261602
Alistair.Young <alistair.young@xero.com>
2022-08-15 20:52:13 +1200

    updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 version = "2021.1"
 
